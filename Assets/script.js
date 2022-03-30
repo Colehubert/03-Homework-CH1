d4a3c8c9fbe8dd411b9b46543593d78e1973da06
Colehubert <Colehubert2001@gmail.com>
2022-03-30 14:23:10 -0700

    making the variable names lowercase

--- Assets/script.js
+++ Assets/script.js
@@ -22,6 +22,53 @@ var specialCharacters = [
 ".",
 ];
 var numbers = ["0", "1", "2", "3","4", "5", "6", "7","8", "9" ]
+var lowercase = [
+"a",
+"b",
+"c",
+"d",
+"e",
+"f",
+"g",
+"h",
+"i",
+"j",
+"k",
+"l",
+"m",
+"n",
+"o",
+"p",
+"q",
+"r",
+"s",
+"t",
+"u",
+"v",
+"w",
+"x",
+"y",
+"z",
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
