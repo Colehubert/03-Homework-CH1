89aa2f7072420bbce81fe3cc135ad03c62afce83
Colehubert <Colehubert2001@gmail.com>
2022-03-30 14:11:37 -0700

    Making the variable special characters and numbers

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,27 @@
-// Assignment Code
+var specialCharacters = [
+"!",
+"@",
+"#",
+"+",
+"//",
+"/",
+"'",
+"$",
+"^",
+"?",
+":",
+",",
+"(",
+")",
+"{",
+"}",
+"[",
+"]",
+"~",
+"-",
+".",
+];
+var numbers = ["0", "1", "2", "3","4", "5", "6", "7","8", "9" ]
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
